d0a1ea719d242fa3a0ea1962d0d0e1ed162e710e
yostane <1958676+yostane@users.noreply.github.com>
2023-12-10 21:48:23 +0100

    Start of day10 in csharp

---
+++ day10-csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using day10;
+
+static Point FindStart(string[] lines)
+{
+    for (int i = 0; i < lines.Length; i++)
+    {
+        var line = lines[i];
+        for (int j = 0; j < line.Length; j++)
+        {
+            if (line[j] == 'S')
+            {
+                return new(i, j);
+            }
+        }
+    }
+    return new();
+}
+
+static int RunStep1(string input)
+{
+    var lines = input.Split("\n");
+    var startPoint = FindStart(lines);
+
+}
+
+
+Console.WriteLine("step 1");
+Console.WriteLine($"test 1. expexted: 4, actual: {runStep1(InputData.TestInput1)}");
+Console.WriteLine($"test 2. expexted: 8, actual: {runStep1(InputData.TestInput2)}");
+Console.WriteLine($"Puzzle Input. Actual: {runStep1(InputData.TestInput1)}");
