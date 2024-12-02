94d4123a63a3aac1ff412b1bddc26e92c5ad3553
yostane <1958676+yostane@users.noreply.github.com>
2024-12-02 17:49:37 +0100

    Day 2 solutions

---
+++ 2024/day2-kotlin/2024-day2.kt
@@ -0,0 +1,37 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//SOURCES input.kt
+
+import kotlin.math.abs
+import kotlin.time.Duration.Companion.seconds
+
+fun isReportSafe(reportLevels: List<Int>): Boolean {
+    val differences = reportLevels.windowed(2).map { it[1] - it[0]  }
+    val areDifferencesInRange = differences.all { abs(it) >= 1 && abs(it) <= 3 }
+    val areOnlyIncreasing = differences.all { it >= 0 }
+    val areOnlyDecreases = differences.all { it <= 0 }
+    return areDifferencesInRange && (areOnlyIncreasing || areOnlyDecreases)
+}
+
+fun isReportSafeWithDampener(reportLevels: List<Int>): Boolean {
+    val dampenedPossibilities = (0..<(reportLevels.size)).map {
+        reportLevels.filterIndexed { index, item -> index != it}
+    }
+    return dampenedPossibilities.any { isReportSafe(it)  }
+}
+
+fun run(input: String) {
+    val lines = input.split("\n")
+    val reports = lines.map { it.split(" ").map { it.toInt() } }
+    val part1Result = reports.count(::isReportSafe)
+    println("part1 $part1Result")
+    val part2Result = + part1Result + reports.filter { !isReportSafe(it)  }.filter(::isReportSafeWithDampener).size
+    println("part2 $part2Result")
+}
+
+fun main() {
+    println("Sample input")
+    run(testInput)
+    println("Puzzle input")
+    run(input)
+}
