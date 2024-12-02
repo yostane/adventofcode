7b50d01e60e414d67bd49406f1d4ff92e0056e7a
yostane <1958676+yostane@users.noreply.github.com>
2024-12-02 17:52:10 +0100

    Day 2 solutions

--- 2024/day2-kotlin/2024-day2.kt
+++ 2024/day2-kotlin/2024-day2.kt
@@ -6,26 +6,24 @@ import kotlin.math.abs
 import kotlin.time.Duration.Companion.seconds
 
 fun isReportSafe(reportLevels: List<Int>): Boolean {
-    val differences = reportLevels.windowed(2).map { it[1] - it[0]  }
+    val differences = reportLevels.windowed(2).map { it[1] - it[0] }
     val areDifferencesInRange = differences.all { abs(it) >= 1 && abs(it) <= 3 }
     val areOnlyIncreasing = differences.all { it >= 0 }
     val areOnlyDecreases = differences.all { it <= 0 }
     return areDifferencesInRange && (areOnlyIncreasing || areOnlyDecreases)
 }
 
-fun isReportSafeWithDampener(reportLevels: List<Int>): Boolean {
-    val dampenedPossibilities = (0..<(reportLevels.size)).map {
-        reportLevels.filterIndexed { index, item -> index != it}
-    }
-    return dampenedPossibilities.any { isReportSafe(it)  }
-}
+fun isReportSafeWithDampener(reportLevels: List<Int>) =
+    (0..<(reportLevels.size)).map {
+        reportLevels.filterIndexed { index, item -> index != it }
+    }.any { isReportSafe(it) }
 
 fun run(input: String) {
     val lines = input.split("\n")
     val reports = lines.map { it.split(" ").map { it.toInt() } }
     val part1Result = reports.count(::isReportSafe)
     println("part1 $part1Result")
-    val part2Result = + part1Result + reports.filter { !isReportSafe(it)  }.filter(::isReportSafeWithDampener).size
+    val part2Result = +part1Result + reports.filter { !isReportSafe(it) }.filter(::isReportSafeWithDampener).size
     println("part2 $part2Result")
 }
 
