ea63cdb56207a197b442db6661b8a5c74a112191
yostane <1958676+yostane@users.noreply.github.com>
2024-12-11 11:02:43 +0100

    day 11 part 2 progress

--- 2024/day11-kotlin/2024-day11.kt
+++ 2024/day11-kotlin/2024-day11.kt
@@ -35,6 +35,24 @@ fun countStonesRecursive(stone: String, currentBlinkCount: Int, maxBlinkCount: I
     }
 }
 
+fun countStonesRecursiveOptimized(stone: String, currentBlinkCount: Int, maxBlinkCount: Int): Int {
+    if (currentBlinkCount == maxBlinkCount) {
+        return 1
+    }
+    return when {
+        stone == "0" -> countStonesRecursive("1", currentBlinkCount + 1, maxBlinkCount)
+        stone.length % 2 == 0 -> {
+            val p1 = stone.substring(0, stone.length / 2).toLong().toString()
+            val h1 = countStonesRecursive(p1, currentBlinkCount + 1, maxBlinkCount)
+            val p2 = stone.substring(stone.length / 2).toLong().toString()
+            val h2 = countStonesRecursive(p2, currentBlinkCount + 1, maxBlinkCount)
+            h1 + h2
+        }
+
+        else -> countStonesRecursive((stone.toLong() * 2024L).toString(), currentBlinkCount + 1, maxBlinkCount)
+    }
+}
+
 fun run(input: String) {
     val intialStones = input.split(" ")
     var stones = intialStones
@@ -46,7 +64,7 @@ fun run(input: String) {
     val part1ResultMethod2 = intialStones.sumOf { countStonesRecursive(it, 0, 25) }
     println("part1 $part1Result - $part1ResultMethod2")
 
-    val part2ResultMethod2 = stones.subList(0, 2).sumOf { countStonesRecursive(it, 25, 75) }
+    val part2ResultMethod2 = stones.sumOf { countStonesRecursive(it, 25, 75) }
     println("part2 $part2ResultMethod2")
 //    for (i in 25..<75) {
 //        stones = blink(stones)
