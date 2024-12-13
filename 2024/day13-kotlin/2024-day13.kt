e2aa5e20f7a7160fdf8c903dd80a20fec8ece527
yostane <1958676+yostane@users.noreply.github.com>
2024-12-13 17:29:37 +0100

    Day 13 part 2 failed attempt

--- 2024/day13-kotlin/2024-day13.kt
+++ 2024/day13-kotlin/2024-day13.kt
@@ -28,37 +28,27 @@ fun Vec2D.isMultipleOfWithSameFactor(vec2D: Vec2D) =
 data class GameAttempt(val aPresses: Long, val bPresses: Long, val cost: Long)
 
 data class Machine(val aMove: Vec2D, val bMove: Vec2D, val target: Vec2D) {
-    val minimalCost: Int?
+    val minimalCost: Long?
     private val maxPressPerButton = 100
 
     init {
         minimalCost = computeMinimalCost2()
     }
 
-    private fun computeCost(aPressCount: Int, bPressCount: Int) = aPressCount * 3 + bPressCount
-
-    private fun minNullable(minCost: Int?, newMinCost: Int?) =
-        when {
-            minCost != null && newMinCost == null -> minCost
-            minCost == null && newMinCost != null -> newMinCost
-            minCost != null && newMinCost != null -> min(minCost, newMinCost)
-            else -> null
-        }
-
     private fun findMinPresses(
         incrementPressMove: Vec2D,
         incrementPressCost: Int,
         otherPressMove: Vec2D,
-        otherPressCost: Int
+        otherPressCost: Int,
     ): MutableSet<GameAttempt> {
         var pressCount = 0L
-        var currentLocation: Vec2D = 0 to 0
+        var currentLocation: Vec2D = 0L to 0L
         val gameAttempts = mutableSetOf<GameAttempt>()
-        while (pressCount < maxPressPerButton && currentLocation.first < target.first && currentLocation.second < target.second) {
+        while ((target.first > 10000000000000 || pressCount < maxPressPerButton) && currentLocation.first < target.first && currentLocation.second < target.second) {
             val remainingDistance = target - currentLocation
             if (remainingDistance.isMultipleOfWithSameFactor(otherPressMove)) {
                 val otherPressCount = remainingDistance.first / otherPressMove.first
-                if (otherPressCount <= 100) {
+                if (target.first > 10000000000000L || otherPressCount <= 100) {
                     val cost = pressCount * incrementPressCost + otherPressCost * otherPressCount
                     val gameAttempt = GameAttempt(pressCount, otherPressCount, cost)
                     gameAttempts.add(gameAttempt)
@@ -70,45 +60,11 @@ data class Machine(val aMove: Vec2D, val bMove: Vec2D, val target: Vec2D) {
         return gameAttempts
     }
 
-    private fun computeMinimalCost2(): Int? {
+    private fun computeMinimalCost2(): Long? {
         val attempts = findMinPresses(aMove, 3, bMove, 1)
         attempts.addAll(findMinPresses(bMove, 1, aMove, 3))
         println(attempts.joinToString("\n"))
         return attempts.minOfOrNull { it.cost }
-    }
-
-    private fun computeMinimalCost(
-        aPressCount: Int = 0,
-        bPressCount: Int = 0,
-        currentLocation: Vec2D = 0 to 0,
-        pressedA: Boolean,
-        currentMinCost: Int? = null
-    ): Int? {
-        if (aPressCount > maxPressPerButton || bPressCount > maxPressPerButton
-            || currentLocation.first > target.first || currentLocation.second > target.second
-        ) {
-            return currentMinCost
-        }
-        if (currentLocation == target) {
-            val cost = computeCost(aPressCount, bPressCount)
-            return minNullable(currentMinCost, cost)
-        }
-        val remainingDistance = target - currentLocation
-        if (pressedA) {
-            if (remainingDistance.isMultipleOfWithSameFactor(bMove)) {
-                val cost = computeCost(aPressCount, remainingDistance.first / bMove.first)
-                println("a: $bPressCount, b: ${remainingDistance.first / aMove.first}, cost = $cost")
-                return minNullable(currentMinCost, cost)
-            }
-            return computeMinimalCost(aPressCount + 1, bPressCount, currentLocation + aMove, true, currentMinCost)
-        } else {
-            if (remainingDistance.isMultipleOfWithSameFactor(aMove)) {
-                val cost = computeCost(remainingDistance.first / aMove.first, bPressCount)
-                println("a: ${remainingDistance.first / aMove.first}, b: $bPressCount, cost = $cost")
-                return minNullable(currentMinCost, cost)
-            }
-            return computeMinimalCost(aPressCount, bPressCount + 1, currentLocation + bMove, false, currentMinCost)
-        }
     }
 
     override fun toString(): String {
@@ -120,14 +76,14 @@ fun Vec2D.isInBox(lineCount: Int, columnCount: Int) =
     this.first in 0..<lineCount && this.second in 0..<columnCount
 
 fun parseButtonLine(line: String): Vec2D {
-    val x = line.substringAfter("X+").substringBefore(", Y+").toInt()
-    val y = line.substringAfter(", Y+").toInt()
+    val x = line.substringAfter("X+").substringBefore(", Y+").toLong()
+    val y = line.substringAfter(", Y+").toLong()
     return x to y
 }
 
 fun parsePrizeLine(line: String): Vec2D {
-    val x = line.substringAfter("X=").substringBefore(", Y=").toInt()
-    val y = line.substringAfter(", Y=").toInt()
+    val x = line.substringAfter("X=").substringBefore(", Y=").toLong()
+    val y = line.substringAfter(", Y=").toLong()
     return x to y
 }
 
@@ -145,7 +101,10 @@ fun run(input: String) {
     val part1Result = machines.sumOf { it.minimalCost ?: 0 }
     println("part1 $part1Result")
 
-    val part2Result = 0
+    val machines2 = machines.map {
+        Machine(it.aMove, it.bMove, it.target.first + 10000000000000L to it.target.second + 10000000000000L)
+    }
+    val part2Result = machines2.sumOf { it.minimalCost ?: 0L }
     println("part2 $part2Result")
 }
 
