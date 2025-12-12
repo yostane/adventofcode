e7d8ca780b1b389313e7e70b8dec12ac9c7e6281
yostane <1958676+yostane@users.noreply.github.com>
2025-12-12 15:37:54 +0100

    Implement Day 2 solution in Kotlin with input parsing and add .NET project files for Day 3

--- 2025/day2-kotlin/2025-day2.kt
+++ 2025/day2-kotlin/2025-day2.kt
@@ -3,11 +3,13 @@
 //KOTLIN 2.1.0
 //SOURCES 2025-day2-input.kt
 
+fun loadIntoRanges(input: String) = input.splitToSequence(",").map {
+    val parts = it.split("-")
+    Pair(parts.first().toLong(), parts.last().toLong())
+}
+
 fun runPart1(input: String): Long{
-    val ranges = input.split(",").map {
-        val parts = it.split("-")
-        Pair(parts.first().toLong(), parts.last().toLong())
-    }
+    val ranges = loadIntoRanges(input)
     val result = ranges.sumOf { pair ->
         (pair.first..pair.second).filter {
             val s = it.toString()
@@ -17,11 +19,27 @@ fun runPart1(input: String): Long{
     return result
 }
 
+fun isIdPart2Invalid(id: String) = (1..id.length / 2).any { subStringLength ->
+    val sequence = id.substring(0, subStringLength)
+    Regex("^(${sequence})+$").matches(id)
+}
+
+
+fun runPart2(input: String): Long {
+    val ranges = loadIntoRanges(input)
+    val result = ranges.sumOf { pair ->
+        (pair.first..pair.second).filter {
+            isIdPart2Invalid(it.toString())
+        }.sum()
+    }
+    return result
+}
+
 fun run(input: String) {
     val part1Result = runPart1(input)
     println("part1 $part1Result")
 
-    val part2Result = 0
+    val part2Result = runPart2(input)
     println("part2 $part2Result")
 }
 
