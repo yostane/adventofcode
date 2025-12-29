df00369dcb0ad3765d8c7c7537d28723b7aed468
yostane <1958676+yostane@users.noreply.github.com>
2025-12-29 17:54:33 +0100

    Implement runStep1 function and update main to process sample inputs

--- 2025/day4-go/main.go
+++ 2025/day4-go/main.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+func runStep1(input string) int {
+	return 0
+}
+
 func main() {
-	for i := range  {
-		
+	for i, v := range SampleInputs {
+		fmt.Printf("Sample input %d\n", i)
+		result := runStep1(v)
+		fmt.Printf("Result %d\n", result)
 	}
 }
